debbc42a0ac3eb222986f9ba71bc1d1d821d3fe6
wilbert <wilberttttt@outlook.com>
2017-07-05 21:31:48 +0700

    FINISHED SIGNUP AND LOGIN
    MISSING : USERNAME AND FULLNAME STILL HARDCODED
    ACTIVATION STILL NEED TO GO VIA URL

---
+++ app/src/main/java/com/example/timotiusek/musikonek/Helper/Connector.java
@@ -0,0 +1,15 @@
+package com.example.timotiusek.musikonek.Helper;
+
+/**
+ * Created by wilbe on 04/07/2017.
+ */
+
+public class Connector {
+
+    private static String URL = "http://192.168.0.106:3000";
+
+    public static String getURL(){
+        return URL;
+    }
+
+}
--- app/src/main/java/com/example/timotiusek/musikonek/SignInActivity.java
+++ app/src/main/java/com/example/timotiusek/musikonek/SignInActivity.java
@@ -3,15 +3,42 @@ package com.example.timotiusek.musikonek;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.example.timotiusek.musikonek.Helper.Connector;
+import com.example.timotiusek.musikonek.Helper.ShaConverter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private final String TAG = "ASDF";
+
     @BindView(R.id.input_email_sign_in)
     EditText inputEmail;
 
@@ -30,8 +57,13 @@ public class SignInActivity extends AppCompatActivity {
 
     @OnClick(R.id.sign_in_btn)
     public void signIn()
-    {   Intent intent = new Intent(SignInActivity.this, MainActivity.class);
-        startActivity(intent);
+    {
+
+        loginCall();
+
+
+
+
 //        if(inputEmail.getText().toString().equals("admin") && inputPassword.getText().toString().equals("admin")){
 //            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
 //            startActivity(intent);
@@ -44,4 +76,119 @@ public class SignInActivity extends AppCompatActivity {
     void goToRegisterPage(){
         startActivity(new Intent(this, SignUpActivity.class));
     }
+
+
+    void loginCall(){
+
+        Log.d("ASDF","Called");
+
+        RequestQueue requestQueue;
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        final Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
+        String url = Connector.getURL() +"/api/v1/studentlogin";
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject res = new JSONObject(response);
+//                            String name = String.valueOf(res.get("name"));
+                            Log.d(TAG, "YOSAH + \n"+res.toString() );
+
+                            if(res.getString("token")!=null){
+
+                                Toast.makeText(SignInActivity.this, "token is "+ res.getString("token"), Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        NetworkResponse networkResponse = error.networkResponse;
+
+                        if(networkResponse == null){
+                        }else{
+                            int a = networkResponse.statusCode;
+                            if(networkResponse.statusCode == 401){
+                            }
+
+                            if(networkResponse.statusCode == 500){
+                                Toast.makeText(SignInActivity.this, "INVALID CREDENTIALS",Toast.LENGTH_SHORT).show();
+                            }
+
+                            if(networkResponse.statusCode != 401){
+
+                                Log.d("ASDF","SHIT");
+
+                            }
+
+                        }
+
+
+
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> reqBody = new HashMap<String, String>();
+
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                try {
+                    password = ShaConverter.SHA1(password);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                reqBody.put("email", email);
+                reqBody.put("password", password);
+
+                return checkParams(reqBody);
+            }
+            private Map<String, String> checkParams(Map<String, String> map){
+                Iterator<Map.Entry<String, String>> it = map.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<String, String> pairs = (Map.Entry<String, String>)it.next();
+                    if(pairs.getValue()==null){
+                        map.put(pairs.getKey(), "");
+                    }
+                }
+                return map;
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                int mStatusCode = response.statusCode;
+                return super.parseNetworkResponse(response);
+            }
+
+        };
+
+//        RequestQueue requestQueue = Volley.newRequestQueue(this);
+//        requestQueue.add(stringRequest);
+
+        requestQueue.add(stringRequest);
+
+    }
+
 }
