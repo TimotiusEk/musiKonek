8cdb7be0ec2158ac22afde13740bc1ea638688a6
wilbert <wilberttttt@outlook.com>
2017-09-21 22:08:43 +0700

    change stuffs, including adding string address

--- app/src/main/java/com/example/timotiusek/musikonek/ShortTestimonialFragment.java
+++ app/src/main/java/com/example/timotiusek/musikonek/ShortTestimonialFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -34,6 +35,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -93,7 +98,126 @@ public class ShortTestimonialFragment extends Fragment {
 
     @OnClick(R.id.open_btn__short_testimonial_fra)
     void openTestimonialInputPage(){
-        Toast.makeText(getActivity(), "getPressed", Toast.LENGTH_SHORT).show();
+        EditText commentEdit = (EditText) getView().findViewById(R.id.edit_comment__short_testimonial_fra);
+        if(commentEdit.getVisibility() == View.INVISIBLE){
+            Toast.makeText(getActivity(), "You've already rated this course", Toast.LENGTH_SHORT).show();
+        }else{
+//            Toast.makeText(getActivity(), "Aw yisss", Toast.LENGTH_SHORT).show();
+            submitForm();
+        }
+    }
+
+    void submitForm(){
+
+        RequestQueue requestQueue;
+        Cache cache = new DiskBasedCache(getActivity().getCacheDir(), 1024 * 1024); // 1MB cap
+        final Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
+        String url = Connector.getURL() +"/api/v1/course/rate";
+        Log.d("ASDF",url);
+
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getActivity(),"Success", Toast.LENGTH_SHORT).show();
+                        EditText commentEdit = (EditText) getView().findViewById(R.id.edit_comment__short_testimonial_fra);
+                        TextView commentView = (TextView) getView().findViewById(R.id.comment__short_testimonial_fra);
+
+                        commentEdit.setVisibility(View.INVISIBLE);
+                        commentView.setVisibility(View.VISIBLE);
+
+                        ratingBar.setIsIndicator(true);
+
+                        commentView.setText(commentEdit.getText());
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        NetworkResponse networkResponse = error.networkResponse;
+
+                        if(networkResponse == null){
+
+                            Toast.makeText(getActivity(), "Connection Error",Toast.LENGTH_SHORT).show();
+
+                        }else{
+                            int a = networkResponse.statusCode;
+                            if(networkResponse.statusCode == 401){
+                            }
+
+                            if(networkResponse.statusCode == 500){
+                                Toast.makeText(getActivity(), "ERROR",Toast.LENGTH_SHORT).show();
+                            }
+
+                            if(networkResponse.statusCode != 401){
+
+
+                            }
+
+                        }
+
+
+
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> reqBody = new HashMap<String, String>();
+
+                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("profile", Context.MODE_PRIVATE);
+
+                String token ="";
+
+                if(!sharedPreferences.getString("token","").equals("")) {
+                    token = sharedPreferences.getString("token","");
+                }
+
+                EditText commentEdit = (EditText) getView().findViewById(R.id.edit_comment__short_testimonial_fra);
+
+                reqBody.put("token",token);
+                reqBody.put("course_id",ta.getCourseID());
+                reqBody.put("rating", String.valueOf(ratingBar.getRating()));
+                reqBody.put("comment",commentEdit.getText().toString());
+
+                return checkParams(reqBody);
+            }
+            private Map<String, String> checkParams(Map<String, String> map){
+                Iterator<Map.Entry<String, String>> it = map.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<String, String> pairs = (Map.Entry<String, String>)it.next();
+                    if(pairs.getValue()==null){
+                        map.put(pairs.getKey(), "");
+                    }
+                }
+                return map;
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                int mStatusCode = response.statusCode;
+                return super.parseNetworkResponse(response);
+            }
+
+        };
+
+//        RequestQueue requestQueue = Volley.newRequestQueue(this);
+//        requestQueue.add(stringRequest);
+
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        requestQueue.add(stringRequest);
+
+
     }
 
     void loadScore(){
@@ -135,6 +259,7 @@ public class ShortTestimonialFragment extends Fragment {
                             }else{
                                 commentView.setText(res.getString("comment"));
                                 commentEdit.setVisibility(View.INVISIBLE);
+                                ratingBar.setIsIndicator(true);
                             }
 
 
