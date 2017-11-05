24259cb3cc36b60509c7afe0b288f4bdc78c09e1
sake1 <timothydickys@gmail.com>
2017-11-06 03:04:20 +0700

    bug fix

--- app/src/main/java/com/example/timotiusek/musikonek/Helper/Connector.java
+++ app/src/main/java/com/example/timotiusek/musikonek/Helper/Connector.java
@@ -6,12 +6,11 @@ package com.example.timotiusek.musikonek.Helper;
 
 public class Connector {
 
-//    private static String URL = "http://192.168.0.107:3000";
+    //SERVER IP
+    private static String URL = "http://162.212.152.65:3000";
 
-//    private static String URL = "http://192.168.0.105:3000";
-//    private static String URL = "http://172.18.1.131:3000";
-//    private static String URL = "http://162.212.152.65:3000";
-    private static String URL = "http://192.168.4.25:3000";
+//    private static String URL = "http://192.168.0.107:3000";
+//    private static String URL = "http://192.168.4.25:3000";
 
     public static String getURL(){
         return URL;
